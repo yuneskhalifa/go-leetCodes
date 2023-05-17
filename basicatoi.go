113c65229d5279151ee9fa9ba7e8d771b1d3f52b
yunes-khalifa <96302174+yunes-khalifa@users.noreply.github.com>
2023-05-17 18:16:53 +0300

    gaigk

--- basicatoi.go
+++ basicatoi.go
@@ -1,9 +1,9 @@
 package piscine
 
-func StrRev(s string) string {
-	rns := []rune(s)
-	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
-		rns[i], rns[j] = rns[j], rns[i]
+func BasicAtoi(s string) int {
+	var result int
+	for _, c := range s {
+		result = result*10 + int(c-'0')
 	}
-	return string(rns)
+	return result
 }
