0c0f91cdec81fc2376b4a14daa6a52ee5c58bf95
yunes-khalifa <96302174+yunes-khalifa@users.noreply.github.com>
2023-05-21 20:11:21 +0300

    kbavjkvkbk

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,13 +1,14 @@
 package piscine
+
 func RecursiveFactorial(nb int) int {
-if nb ==1 || nb ==0{
-	return 1
-}
-if nb >=25 || nb=0{
-	return 1
-}
-if nb >= 25 ||nb <0{
-	return 0 
-}
-return (nb * RecursiveFactorial(nb-1))
+	if nb == 1 || nb == 0 {
+		return 1
+	}
+	if nb >= 25 || nb == 0 {
+		return 1
+	}
+	if nb >= 25 || nb < 0 {
+		return 0
+	}
+	return (nb * RecursiveFactorial(nb-1))
 }
