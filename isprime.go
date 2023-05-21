155901f716f9a601b0fdf8c1c2d71b305bbbf8be
yunes-khalifa <96302174+yunes-khalifa@users.noreply.github.com>
2023-05-21 23:49:46 +0300

    kbavjkvkbk

--- isprime.go
+++ isprime.go
@@ -1,11 +1,11 @@
 package piscine
 
 func IsPrime(nb int) bool {
-	if n <= 1 {
+	if nb <= 1 {
 		return false
 	}
-	for i := 2; i*i <= n; i++ {
-		if n%i == 0 {
+	for i := 2; i*i <= nb; i++ {
+		if nb%i == 0 {
 			return false
 		}
 	}
