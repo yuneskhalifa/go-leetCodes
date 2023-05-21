1df0c1c85e96335c4189b97c7ec6ce4325839947
yunes-khalifa <96302174+yunes-khalifa@users.noreply.github.com>
2023-05-21 18:41:10 +0300

    vhlsdvbgk

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,8 +3,17 @@ package piscine
 func IterativeFactorial(nb int) int {
 	factorial := 1
 
-	for i := 1; i <= nb; i++ {
-		factorial = factorial * i
+	if nb >= 1 && nb<=25{
+		for i :=nb; i>=1;i--{
+			factorial*=i
+		}
+		return x
 	}
-	return factorial
+	if nb >=26 || nb<0{
+		return 0
+	}
+	if nb==0{
+		return 1
+	}
+	return 0
 }
