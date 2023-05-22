8091029d5f02272bdbd2c4d251e7bf2730b7525a
yunes-khalifa <96302174+yunes-khalifa@users.noreply.github.com>
2023-05-22 20:49:30 +0300

    hsdkbkvj

--- index.go
+++ index.go
@@ -1,11 +1,23 @@
 package piscine
 
 func Index(s, toFind string) int {
-	for i := 0; i < len(s); i++ {
-		if s[i:i+len(toFind)] == toFind {
+	a := len(s)
+	suba := len(toFind)
+	t := 0
+	for i := 0; i < a; i++ {
+		j := 0
+		t = i
+		for j < suba {
+			if t < a && s[t] == toFind[j] {
+				j++
+				t++
+			} else {
+				break
+			}
+		}
+		if j == suba {
 			return i
 		}
 	}
-
 	return -1
 }
