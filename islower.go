17bbd66f2a220a035310d3247ff825b13fab98e1
yunes-khalifa <96302174+yunes-khalifa@users.noreply.github.com>
2023-05-23 15:04:10 +0300

    hsdkbkvj

--- islower.go
+++ islower.go
@@ -1,13 +1,11 @@
 package piscine
 
 func IsLower(s string) bool {
-	func isLowercase(s string) bool {
-		for _, r := range s {
-		  if r < 'a' || r > 'z' {
-			return false
-		  }
+	for _, r := range s {
+		if r < 'a' || r > 'z' {
+		  return false
 		}
-		return true
 	  }
+	  return true
 }
 		
