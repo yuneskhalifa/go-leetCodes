da6b73a410858fff9dc107634e047d00d0260b04
yunes-khalifa <96302174+yunes-khalifa@users.noreply.github.com>
2023-05-23 18:18:33 +0300

    lhsdkbkvb

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -1,22 +1,24 @@
-package piscine 
+package piscine
+
 import "fmt"
+
 func PrintNbrInOrder(n int) {
 	var digits []int
-  for n > 0 {
-    digits = append(digits, n % 10)
-    n /= 10
-  }
-  for i := 0; i < len(digits); i++ {
-    for j := i + 1; j < len(digits); j++ {
-      if digits[i] > digits[j] {
-        var temp int
-        temp = digits[i]
-        digits[i] = digits[j]
-        digits[j] = temp
-      }
-    }
-  }
-  for _, d := range digits {
-    fmt.Print(d)
-  }
+	for n > 0 {
+		digits = append(digits, n%10)
+		n /= 10
+	}
+	for i := 0; i < len(digits); i++ {
+		for j := i + 1; j < len(digits); j++ {
+			if digits[i] > digits[j] {
+				var temp int
+				temp = digits[i]
+				digits[i] = digits[j]
+				digits[j] = temp
+			}
+		}
+	}
+	for _, d := range digits {
+		fmt.Print(d)
+	}
 }
