c19637f1248ae108471f1cb9443b83489088b1e0
yunes-khalifa <96302174+yunes-khalifa@users.noreply.github.com>
2023-05-23 19:13:48 +0300

    lhsdkbkvb

--- capitalize.go
+++ capitalize.go
@@ -2,9 +2,13 @@ package piscine
 
 func Capitalize(s string) string {
 	var result []rune
-	for _, r := range s {
-		if unicode.IsLetter(r) {
-			result = append(result, unicode.ToUpper(r))
+	for i, r := range s {
+		if r >= 'a' && r <= 'z' {
+			if i == 0 || s[i-1] < 'a' || s[i-1] > 'z' {
+				result = append(result, unicode.ToUpper(r))
+			} else {
+				result = append(result, r)
+			}
 		} else {
 			result = append(result, r)
 		}
