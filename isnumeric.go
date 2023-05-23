59b90254a5af231224c3667ce525b673d1a8388c
yunes-khalifa <96302174+yunes-khalifa@users.noreply.github.com>
2023-05-23 16:39:01 +0300

    hsdkbkvj

--- isnumeric.go
+++ isnumeric.go
@@ -3,9 +3,8 @@ package piscine
 func IsNumeric(s string) bool {
 	for _, r := range s {
 		if r < '0' || r > '9' {
-			  return false
+			return false
 		}
-	  }
-	  return true
+	}
+	return true
 }
-	
