d49829e5ac96425430936b78399374d9d7f5aec6
yunes-khalifa <96302174+yunes-khalifa@users.noreply.github.com>
2023-05-24 22:04:56 +0300

    kbvkbv

---
+++ revparams/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+	arg := os.Args[1:]
+	arglen := len(arg) - 1
+	for i := arglen; i >= 0; i-- {
+		for _, j := range arg[i] {
+			z01.PrintRune(j)
+		}
+		z01.PrintRune('\n')
+	}
+}
