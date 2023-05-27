ea4f1928350a4a991fac611ada35fda1f80cf9a0
yunes-khalifa <96302174+yunes-khalifa@users.noreply.github.com>
2023-05-28 01:38:39 +0300

    lkvn

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -3,10 +3,16 @@ package piscine
 func SplitWhiteSpaces(s string) []string {
 	words := []string{}
 	for _, rune := range s {
-		if rune == ' ' || rune == '\t' || rune == '\n' {
-			continue
+		if rune != ' ' && rune != '\t' && rune != '\n' {
+			start := 0
+			for _, rune := range s[start:] {
+				if rune == ' ' || rune == '\t' || rune == '\n' {
+					break
+				}
+				start++
+			}
+			words = append(words, s[start:start+1])
 		}
-		words = append(words, string(rune))
 	}
 	return words
 }
