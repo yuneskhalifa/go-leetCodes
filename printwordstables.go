a7621626c51fcc21b2c980550ff5d9da4cf8ef27
yunes-khalifa <96302174+yunes-khalifa@users.noreply.github.com>
2023-05-28 20:15:44 +0300

    kvbb

--- printwordstables.go
+++ printwordstables.go
@@ -4,6 +4,9 @@ import "github.com/01-edu/z01"
 
 func PrintWordsTables(a []string) {
 	for _, word := range a {
-		z01.PrintRune(rune word)
+		for _, ch := range word {
+			z01.PrintRune(ch)
+		}
+		z01.PrintRune('\n')
 	}
 }
