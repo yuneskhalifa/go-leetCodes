e5d4adfb2b4dc23142c5995b49acd352389bd26e
yunes-khalifa <96302174+yunes-khalifa@users.noreply.github.com>
2023-05-29 23:37:05 +0300

    lsnvl

--- printcomb2.go
+++ printcomb2.go
@@ -4,16 +4,18 @@ import "github.com/01-edu/z01"
 
 func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
-		for j := '0'; i < '9'; i++ {
-			for k := '0'; k < '9'; k++ {
-				for l := '1'; l < '9'; l++ {
-					z01.PrintRune(i)
+		for j := '0'; i <= '9'; i++ {
+			for k := '0'; k <= '9'; k++ {
+				for l := '0'; l <= '9'; l++ {
+					if i != j || j!=k || k!=l || l != i {
+						z01.PrintRune(i)
 					z01.PrintRune(j)
 					z01.PrintRune(' ')
 					z01.PrintRune(k)
 					z01.PrintRune(l)
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
+					}
 				}
 			}
 		}
