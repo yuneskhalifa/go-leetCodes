f1d147541172593b2e6feb7901a47044a0df9d26
yunes-khalifa <96302174+yunes-khalifa@users.noreply.github.com>
2023-05-30 18:15:30 +0300

    lbvk

---
+++ map.go
@@ -0,0 +1,13 @@
+package piscine
+
+func Map(f func(int) bool, a []int) []bool {
+	ad := []bool{}
+	for i := 0; i < len(a); i++ {
+		if IsPrime(a[i]) == true {
+			ad = append(ad, true)
+		} else {
+			ad = append(ad, false)
+		}
+	}
+	return ad
+}
