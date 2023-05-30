e416ad5a06ca9a9608fa90135f3369a3b3df1cf6
yunes-khalifa <96302174+yunes-khalifa@users.noreply.github.com>
2023-05-30 18:19:59 +0300

    lbvk

--- map.go
+++ map.go
@@ -2,12 +2,8 @@ package piscine
 
 func Map(f func(int) bool, a []int) []bool {
 	ad := []bool{}
-	for i := 0; i < len(a); i++ {
-		if f(a[i]) == true {
-			ad = append(ad, true)
-		} else {
-			ad = append(ad, false)
-		}
+	for _, elem := range a {
+		ad = append(a, f(elem))
 	}
 	return ad
 }
