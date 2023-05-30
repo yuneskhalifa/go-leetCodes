e36ff4d57e9ac8f844e4eefcdedf16598a60137d
yunes-khalifa <96302174+yunes-khalifa@users.noreply.github.com>
2023-05-30 21:10:03 +0300

    kbvk

--- issorted.go
+++ issorted.go
@@ -4,7 +4,10 @@ func IsSorted(f func(a, b int) int, a []int) bool {
 	for i := 0; i < len(a); i++ {
 		if f(a[i], a[i+1]) > 0 {
 			return true
-		} else if f(a[i], a[i+1]) < 0 {
+		}
+	}
+	for j := 0; j < len(a); j++ {
+		if f(a[j], a[j+1]) < 0 {
 			return true
 		}
 	}
