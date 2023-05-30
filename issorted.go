75b5c4c2d64400b464236f89d1516bbfa2656c28
yunes-khalifa <96302174+yunes-khalifa@users.noreply.github.com>
2023-05-30 20:46:47 +0300

    kbvk

--- issorted.go
+++ issorted.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	for i := 0; i < len(a); i++ {
-		if f(a[i], a[i+1]) < 0 {
+		if f(a[i], a[i+1]) < 0 || f(a[i+1], a[i]) < 0 {
 			return false
 		}
 	}
