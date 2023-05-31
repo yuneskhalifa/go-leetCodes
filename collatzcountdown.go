5ef7a68624edd533f51fe6159c70ca0b00092c18
yunes-khalifa <96302174+yunes-khalifa@users.noreply.github.com>
2023-05-31 14:59:35 +0300

    fhf

--- collatzcountdown.go
+++ collatzcountdown.go
@@ -2,7 +2,7 @@ package piscine
 
 func CollatzCountdown(start int) int {
 	counter := 0
-	for i := start; i > 3; i-- {
+	for i := start; i > 1; i-- {
 		if start == 0 || start < 0 {
 			return -1
 		}
