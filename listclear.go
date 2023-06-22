a23da87f497fda0ba5681b00b9cbb3c4afed9e7e
yunes-khalifa <96302174+yunes-khalifa@users.noreply.github.com>
2023-06-22 12:40:13 +0300

    kbsdkb

--- listclear.go
+++ listclear.go
@@ -1,10 +1,6 @@
-package piscine
+package main
 
 func ListClear(l *List) {
-	current := l.Head
-	for current != nil {
-		next := current.Next
-		current = nil
-		current = next
-	}
+	l.Head = nil
+	l.Tail = nil
 }
