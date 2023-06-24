de78f709f636199240c028036fb51e3ac3c91da1
yunes-khalifa <96302174+yunes-khalifa@users.noreply.github.com>
2023-06-24 16:28:23 +0300

    lsnn

--- btreeapplypreorder.go
+++ btreeapplypreorder.go
@@ -1,9 +1,10 @@
 package piscine
 
 func BTreeApplyPreorder(root *TreeNode, f func(...interface{}) (int, error)) {
-	if root != nil {
-		f(root.Data)
-		BTreeApplyInorder(root.Left, f)
-		BTreeApplyInorder(root.Right, f)
+	if root == nil {
+		return
 	}
+	f(root.Data)
+	BTreeApplyPreorder(root.Left, f)
+	BTreeApplyPreorder(root.Right, f)
 }
