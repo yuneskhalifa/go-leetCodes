ecc26fae9ad2355bfa45bdfba42bd24bb6a9b595
yunes-khalifa <96302174+yunes-khalifa@users.noreply.github.com>
2023-06-24 16:24:36 +0300

    snl

--- btreeapplypostorder.go
+++ btreeapplypostorder.go
@@ -1,32 +1,10 @@
 package piscine
 
 func BTreeApplyPostorder(root *TreeNode, f func(...interface{}) (int, error)) {
-	h := BTreeLevelCount(root)
-	for i := 0; i < h; i++ {
-		applyLevel(root, i, f)
-	}
-}
-
-func applyLevel(root *TreeNode, level int, f func(...interface{}) (int, error)) {
 	if root == nil {
 		return
 	}
-	if level == 0 {
-		f(root.Data)
-	} else if level > 0 {
-		applyLevel(root.Left, level-1, f)
-		applyLevel(root.Right, level-1, f)
-	}
-}
-
-func BTreeLevelCount(root *TreeNode) int {
-	if root == nil {
-		return 0
-	}
-	left := BTreeLevelCount(root.Left)
-	right := BTreeLevelCount(root.Right)
-	if left > right {
-		return left + 1
-	}
-	return right + 1
+	BTreeApplyPostorder(root.Left, f)
+	BTreeApplyPostorder(root.Right, f)
+	f(root.Data)
 }
