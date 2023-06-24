af17107e56ffd83b734c8854c70dc3c3324d67f5
yunes-khalifa <96302174+yunes-khalifa@users.noreply.github.com>
2023-06-24 15:33:04 +0300

    kbbvk

--- listforeachif.go
+++ listforeachif.go
@@ -30,6 +30,15 @@ func IsNotNumeric_node(node *NodeL) bool {
 	return true
 }
 
+func IsAlNode(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return false
+	default:
+		return true
+	}
+}
+
 func ListForEachIf(l *List, f func(*NodeL), cond func(*NodeL) bool) {
 	a := l.Head
 	for a != nil {
